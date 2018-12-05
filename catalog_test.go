20af1be60fe95e936ee07df0c632c917142b339c
Jon Borgonia <jon@gomagames.com>
2018-12-05 15:38:12 -0800

    move and test GetCatalogByID

--- catalog_test.go
+++ catalog_test.go
@@ -10,3 +10,26 @@ func TestGetCatalogs(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestGetCatalogByID(t *testing.T) {
+	setup()
+
+	catalogList, err := client.GetCatalogs("", "")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(catalogList.Members) == 0 {
+		t.Error("TestGetCatalogByID Could not find any Catalogs")
+		return
+	}
+
+	testId := catalogList.Members[0].ID
+	catalog, err := client.GetCatalogByID(testId, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if catalog.ID == "" {
+		t.Errorf("TestGetCatalogByID Failed to get catalog: ID is ''")
+	}
+}
