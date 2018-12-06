d725543e775fd0cf63305f0a9540ca8ffc9871b5
Jon Borgonia <jon@gomagames.com>
2018-12-05 18:47:58 -0800

    move and test GetProjectByID

--- project_test.go
+++ project_test.go
@@ -10,3 +10,26 @@ func TestGetProjects(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestGetProjectByID(t *testing.T) {
+	setup()
+
+	projectList, err := client.GetProjects("", "")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(projectList.Members) == 0 {
+		t.Error("TestGetProjectByID Could not find any Projects")
+		return
+	}
+
+	testId := projectList.Members[0].ID
+	project, err := client.GetProjectByID(testId, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if project.ID == "" {
+		t.Errorf("TestGetProjectByID Failed to get project: ID is ''")
+	}
+}
