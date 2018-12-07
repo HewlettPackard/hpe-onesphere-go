b46e92428a8c3333345acfac07467e16c355783b
Jon Borgonia <jon@gomagames.com>
2018-12-07 11:22:23 -0800

    move and test GetUserByID

--- user_test.go
+++ user_test.go
@@ -10,3 +10,26 @@ func TestGetUsers(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestGetUserByID(t *testing.T) {
+	setup()
+
+	userList, err := client.GetUsers("")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(userList.Members) == 0 {
+		t.Error("TestGetUserByID Could not find any Users")
+		return
+	}
+
+	testId := userList.Members[0].ID
+	user, err := client.GetUserByID(testId)
+	if err != nil {
+		t.Error(err)
+	}
+	if user.ID == "" {
+		t.Errorf("TestGetUserByID Failed to get user: ID is ''")
+	}
+}
